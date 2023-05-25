f6a1e9dd3e0a43ea1814a909bca4d7d47479a3be
FatimaZahraOukmi <Fatimazahra.oukmi@gmail.com>
2023-05-25 04:29:53 -0700

    up

--- Env_vs_environ.h
+++
@@ -1,7 +0,0 @@
-#ifndef ENV_VS_ENVIRON_H
-#define ENV_VS_ENVIRON_H
-
-extern int someVariable;
-extern void someFunction(void);
-
-#endif
--- printenv_with_environ.h
+++
@@ -1,6 +0,0 @@
-#ifndef PRINTENV_WITH_ENVIRON_H
-#define PRINTENV_WITH_ENVIRON_H
-
-extern char **environ;
-
-#endif
